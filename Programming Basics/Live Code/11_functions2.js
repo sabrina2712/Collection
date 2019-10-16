775b4ea30c55c9a887884103faacccf3b1f91277
Oliver Weber <oliver@mondough.io>
2019-10-16 14:15:33 +0200

    Comparing Functions & Light testing (optional)

--- Programming Basics/Live Code/11_functions2.js
+++ Programming Basics/Live Code/11_functions2.js
@@ -41,3 +41,74 @@ const transmitOrderToRestaurant = order => {
 const userInput = "Salami";
 const order = createPizza(userInput);
 transmitOrderToRestaurant(order);
+
+/**
+ * Function Expression vs Arrow Functions
+ *
+ */
+
+function funcExpressionNamed(paramter) {
+  return "Return Value";
+}
+
+const funcExpression = function(parameter) {
+  return "Return Value";
+};
+
+const arrowFunction = paramter => {
+  return "Return Value";
+};
+
+//arrow one-liner (implicit return)
+const arrowFunc = paramter => "Return Value";
+
+/**
+ * Light Testing (Light Unit Testing)
+ * *optional*
+ */
+
+// Create a function that accepts dish kind and 3 ingredients (order form most to least)
+// Dish description
+// 1. kind: Soup, ingredients: Potato, Chicken, Garlic
+// -> "Potato Soup:
+//      with chicken and garlic"
+// 2. kind: Pizza, ingredients: Tuna, Onions, Basil
+// -> "Tuna Pizza:
+//     with onions and basil"
+
+// We could create a snippet out of this 🤔
+const test = (value, expectedResult) => {
+  if (value === expectedResult) {
+    console.log("✅");
+    return;
+  }
+
+  return console.log(`❌ Expected: 
+              ${expectedResult}
+but recieved: 
+              ${value}`);
+};
+
+const dishDescription = (kind, ing1, ing2, ing3) => {
+  ing2 = ing2.toLowerCase();
+  ing3 = ing3.toLowerCase();
+
+  return `${ing1} ${kind}: with ${ing2} and ${ing3}`;
+};
+//  const dishDescription = (kind, ing1, ing2,ing3) => {
+
+//  }
+
+test(
+  dishDescription("Soup", "Potato", "Chicken", "Garlic"),
+  "Potato Soup: with chicken and garlic"
+);
+
+test(
+  dishDescription("Pizza", "Tuna", "Onions", "Basil"),
+  "Tuna Pizza: with onions and basil"
+);
+
+/**
+ * testing *optional* END
+ */
