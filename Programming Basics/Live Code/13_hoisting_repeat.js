90eb10577100c82ff8b4467cbb9c5cb961c816cf
Oliver Weber <oliver@mondough.io>
2019-10-22 15:17:46 +0200

    repeat hoisting

---
+++ Programming Basics/Live Code/13_hoisting_repeat.js
@@ -0,0 +1,12 @@
+function add(num1, num2) {
+  var inner = "hello";
+  return num1 + num2;
+}
+
+var hello;
+console.log(hello, add());
+hello = "Good Morning";
+console.log(variable);
+var variable = () => {
+  return "Hi";
+};
