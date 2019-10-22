641862d2b161a40622fa0d48e90aadfe5873b5c5
Oliver Weber <oliver@mondough.io>
2019-10-22 15:17:52 +0200

    riddle

---
+++ Programming Basics/Live Code/riddles.js
@@ -0,0 +1,60 @@
+/**
+ * Riddle
+ * 18.10.2019
+ * Write a "add" function that works in the following ways:
+ * add(10,5) -> 15
+ * add(10)(5) -> 15
+ *
+ * Concept: Currying
+ * - Clojure
+ * - Scope
+ */
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(add(10, 5) === 15);
+
+const add2 = num1 => {
+  return num2 => {
+    return num1 + num2;
+  };
+};
+
+console.log(add2(10)(5) === 15);
+
+const addCombined = (num1, num2) => {
+  const notNum2 = !num2;
+  if (notNum2) {
+    return num2 => {
+      return num1 + num2;
+    };
+  }
+
+  return num1 + num2;
+};
+
+console.log(addCombined(10, 5) === 15);
+console.log(addCombined(10)(5) === 15);
+
+/**
+ * Compose Functions
+ */
+
+const greet = (greeting, name) => {
+  return greeting + " " + name;
+};
+
+console.log(greet("Hello", "Oliver"));
+// in short: const createGreeting = greeting => name => `${greeting} ${name}`;
+const createGreeting = greeting => {
+  return function(name) {
+    return `${greeting} ${name}`;
+  };
+};
+
+const germanGreeting = createGreeting("Hallo"); // looks like (name) => `Hallo ${name}`
+const englishGreeting = createGreeting("Hello");
+
+console.log(germanGreeting("Bob"), englishGreeting("Tania"));
