bf2b94dedf411ada8b75a90b8f59161b780e5d5f
Oliver Weber <oliver@mondough.io>
2020-02-05 10:22:47 +0100

    uncomment prev code

--- Node/Live Code/04_pass_by_reference/index.js
+++ Node/Live Code/04_pass_by_reference/index.js
@@ -1,58 +1,58 @@
 // // Objects (Arrays) and Primitive values behave differently in comparisons
 // //           1      2
-// console.log({} === {}); // 🤷🏽‍♂️ why
+console.log({} === {}); // 🤷🏽‍♂️ why
 
-// const person = {
-//   name: "Peter",
-//   age: 55
-// };
+const person = {
+  name: "Peter",
+  age: 55
+};
 
-// const changeAge = (person) => {
-//   person.age = 17;
+const changeAge = (person) => {
+  person.age = 17;
 
-//   return person;
-// };
+  return person;
+};
 
-// changeAge(person);
+changeAge(person);
 
-// console.log(person);
+console.log(person);
 
-// // Comparison of objects
-// //  MemId = 5
-// let person1 = {
-//   name: "Joe",
-//   age: 15
-// };
+// Comparison of objects
+//  MemId = 5
+let person1 = {
+  name: "Joe",
+  age: 15
+};
 
-// //  MemId = 6
-// let person2 = {
-//   age: 15,
-//   name: "Joe"
-// };
-// //                5 === 6
-// console.log(person1 === person2);
-// console.dir(JSON.parse(JSON.stringify(person1)));
-// console.log("HERE:", JSON.stringify(person1) === JSON.stringify(person2));
+//  MemId = 6
+let person2 = {
+  age: 15,
+  name: "Joe"
+};
+//                5 === 6
+console.log(person1 === person2);
+console.dir(JSON.parse(JSON.stringify(person1)));
+console.log("HERE:", JSON.stringify(person1) === JSON.stringify(person2));
 
-// {
-//   let person1 = {
-//     name: "Josephine",
-//     age: 15,
-//     hello: true
-//   };
+{
+  let person1 = {
+    name: "Josephine",
+    age: 15,
+    hello: true
+  };
 
-//   //  MemId = 6
-//   let person2 = {
-//     age: 15,
-//     name: "Joe"
-//   };
+  //  MemId = 6
+  let person2 = {
+    age: 15,
+    name: "Joe"
+  };
 
-//   let mergedPerson = Object.assign({}, person1, person2, { age: false });
-//   console.log(mergedPerson === person1, mergedPerson);
+  let mergedPerson = Object.assign({}, person1, person2, { age: false });
+  console.log(mergedPerson === person1, mergedPerson);
 
-//   let mergedPerson2 = { ...person1, ...person2, ...{ age: false } };
-//   console.log(mergedPerson2);
-// }
+  let mergedPerson2 = { ...person1, ...person2, ...{ age: false } };
+  console.log(mergedPerson2);
+}
 
 {
   // What is happening here:
